a76cc7cd0b5afc4f85a4f257ca7fa59b845bb292
tanphuccgl <phamnguyentanphucgl@gmail.com>
2021-12-07 01:45:40 +0700

    [FIX] api edit exercise
    post mảng file keep

--- lib/feature/exercise/data/data_source/edit_exercise.dart
+++ lib/feature/exercise/data/data_source/edit_exercise.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:thuc_tap_tot_nghiep/core/config/constants.dart';
@@ -10,26 +13,46 @@ http.Client? client = http.Client();
 
 Future<bool> editExercise(
     {int? idExercise,
-      String? idCourse,
-      String? titleExercise,
-      String? descriptionExercise,
-      String? allowSubmission,
+    String? idCourse,
+    String? titleExercise,
+    String? descriptionExercise,
+    String? allowSubmission,
     String? submissionDeadline,
-      List<Files>? fileKeep,
-      List<PlatformFile>? listFile,
-      Function? success,
-      Function? failure}) async {
-  print(idCourse);
+    List<Files>? fileKeep,
+    List<PlatformFile>? listFile,
+    Function? success,
+    Function? failure}) async {
+  print("3 $descriptionExercise ");
+  print("to $submissionDeadline");
+
+  // print("file ${listFile!.first}");
+  //print("filekeep  ${fileKeep!.toList()}");
+  print("id courser $idCourse");
+
   ///chua chinh link api
-  var uri = Uri.parse('$mainUrl/lecture/OpenLecture?idCourse=$idCourse');
+  var uri = Uri.parse('$mainUrl/exercise/EditExercise');
   var request = http.MultipartRequest('PUT', uri);
+
+  /// warning
+  var ab = json.encode(fileKeep);
+  // log("haha $ab");
+  //  request.fields["fileKeep"] = ab;
+  // log("hahuu ${ request.fields["fileKeep"]}");
+
+  for (var item in fileKeep!) {
+    request.fields["fileKeep[0]"] = "${item.filename!}";
+    print("hihi ${request.fields["fileKeep[0]"]}");
+  }
 
   request.headers["Accept"] = "application/json";
   request.headers["auth-token"] = "${appUser?.token}";
   request.headers["Content-Type"] = "multipart/form-data";
 
+  // request.headers["Content-Type"] = "application/json";
+
   request.fields["titleExercise"] = "$titleExercise";
   request.fields["idExercise"] = "$idExercise";
+  print("aa ${request.fields["idExercise"]}");
 
   if (allowSubmission != null) {
     request.fields["allowSubmission"] = "$allowSubmission";
@@ -39,21 +62,16 @@ Future<bool> editExercise(
   }
 
   if (descriptionExercise != null) {
-    request.fields["descriptionAnswer"] = "$descriptionExercise";
+    request.fields["descriptionExercise"] = "$descriptionExercise";
   }
-  /// warning
-  request.fields["fileKeep"] = "$fileKeep";
 
-  request.fields["files"] = "files";
   request.fields['idCourse'] = '$idCourse';
+  request.fields["files"] = "files";
+
   for (var item in listFile!) {
     var file = await http.MultipartFile.fromPath("files", item.path!);
     request.files.add(file);
   }
-  // for (var item in listFile!) {
-  //   var file = await http.MultipartFile.fromPath("fileKeep", item.path!);
-  //   request.files.add(file);
-  // }
 
   var response = await request.send();
   var a = await response.stream.toBytes();
